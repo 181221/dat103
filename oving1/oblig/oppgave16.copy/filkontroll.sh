3fcbde5d030c19ce80627395bc473ebaec90a495
h181221 <peder.wiig@gmail.com>
2017-09-05 04:04:01 -0400

    ny commit

--- oving1/oblig/oppgave16.copy/filkontroll.sh
+++ oving1/oblig/oppgave16.copy/filkontroll.sh
@@ -1,25 +1,28 @@
+#!/bin/bash
+
 FIL=$1
 INTERVAL=$2
 
 if [ ! -f $FIL ];
 then
-touch $FIL
-echo "filen $FIL ble opprettet"
+  touch $FIL
+  echo "filen $FIL ble opprettet"
 exit 0
 fi
-	GAMMELFIL=$(stat $FIL -c%Y)
-	while true; do
-	   sleep $INTERVAL
-	   if [ ! -f $FIL ];then
-		echo "Filen $FIL ble slettet"
-		exit 0
-	   else
-		NYFIL=$(stat $FIL -c%Y)
- 		if [[ $GAMMELFIL != $NYFIL ]];
-		then
-			echo "Filen $FIL ble endret."
-			GAMMELFIL=$(stat $FIL -c%Y)
-		fi
-	   fi
+
+GAMMELFIL=$(stat $FIL -c%Y)
+
+while true; do
+   sleep $INTERVAL
+   if [ ! -f $FIL ]; then
+     echo "Filen $FIL ble slettet"
+     exit 0
+   else
+     NYFIL=$(stat $FIL -c%Y)
+     if [[ $GAMMELFIL != $NYFIL ]]; then
+       echo "Filen $FIL ble endret."
+       GAMMELFIL=$(stat $FIL -c%Y)
+     fi
+   fi
 done
 
